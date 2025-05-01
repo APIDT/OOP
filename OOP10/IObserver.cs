629e12ad6e4a6e4174a44d1bba09823ee5241c9c
Bend <144515341+APIDT@users.noreply.github.com>
2025-05-01 08:56:13 +0300

    Delete OOP10 directory

--- OOP10/IObserver.cs
+++
@@ -1,7 +0,0 @@
-﻿public interface IObserver
-{
-    void Update(string stockName, double newPrice);
-    void SubscribeToStock(string stockName);
-    void UnsubscribeFromStock(string stockName);
-    List<string> GetSubscribedStocks();
-}
